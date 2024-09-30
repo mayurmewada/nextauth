68e0caa0e1e0a5644258a3c11745a38985f03ffd
mayurmewada <mayurmewada00@gmail.com>
2024-09-30 23:54:48 +0530

    new:redux added

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
         <div className="flex flex-col items-center justify-center h-[100vh]">
             <h1 className="mb-4">{t("loginToNextAuth")}</h1>
             <p className="mb-7">{t("allFieldRequired")}</p>
-            <form action="post" className="flex flex-col gap-5">
+            <form className="flex flex-col gap-5">
                 <div className="flex flex-col">
                     <label htmlFor="email">{t("formEmailLabel")}</label>
                     <input className="border border-[#dddddd]" type="text" name="email" />
@@ -17,6 +17,9 @@ const Page = () => {
                 <div className="flex flex-col">
                     <label htmlFor="password">{t("formPasswordLabel")}</label>
                     <input className="border border-[#dddddd]" type="password" name="password" />
+                </div>
+                <div className="flex flex-col">
+                    <button type="submit" className="rounded-[4px] border-[1px] border-slate-300 hover:border-slate-400">Login</button>
                 </div>
             </form>
         </div>
---
+++ src/app/api/admin/users/route.ts
@@ -0,0 +1,20 @@
+//module imports
+//file imports
+import { dbConnect } from "@/db/connection";
+import userModel from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+dbConnect();
+
+export const GET = async (request: NextRequest, response: NextResponse) => {
+    try {
+        const getAllUsers = await userModel.find()
+        console.log("getAllUsers", getAllUsers)
+        return NextResponse.json({
+            status: 200,
+            response: getAllUsers
+        })
+    } catch (error) {
+        console.log("error from admin/users/route",error)
+    }
+}

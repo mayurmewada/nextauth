4a7c16096f5f0b0c73484a4158ea0ba0600e27e5
mayurmewada <mayurmewada00@gmail.com>
2024-10-04 22:23:17 +0530

    fix: middleware-protected-routes

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,17 +1,4 @@
-"use client";
-import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 const Page = () => {
-  const router = useRouter();
-  const isAuthenticated = Cookies.get("token");
-
-    useEffect(() => {
-        if (!isAuthenticated) {
-            router.replace("/login");
-        }
-    }, []);
 
     return <div className="container">hello protected admin</div>;
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,30 @@
 // module imports
 import createMiddleware from "next-intl/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export default createMiddleware({
-    // A list of all locales that are supported
+const intlMiddleware = createMiddleware({
     locales: ["en", "hi"],
-
-    // Used when no locale matches
     defaultLocale: "en",
 });
 
+export function middleware(request: NextRequest) {
+    const isCookie = request.cookies.get("token");
+    const cookie = request.cookies.get("NEXT_LOCALE");
+    console.log(isCookie, cookie);
+    const response = intlMiddleware(request);
+
+    // Protected route logic
+    const isProtectedRoute = request.nextUrl.pathname.split("/")[2] == "admin";
+
+    if (isProtectedRoute && !isCookie) {
+        const locale = request.nextUrl.pathname.startsWith("/hi") ? "hi" : "en";
+        return NextResponse.redirect(new URL(`/${locale}/login`, request.url));
+    }
+
+    return response;
+}
+
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ["/", "/(hi|en)/:path*", "/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    matcher: ["/admin", "/", "/(hi|en)/:path*", "/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };

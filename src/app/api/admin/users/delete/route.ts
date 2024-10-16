19fdf7a2ad3e522be118bc32baf41050d861d5ce
mayurmewada <mayurmewada00@gmail.com>
2024-10-16 23:14:55 +0530

    fix: delete api

--- src/app/api/admin/users/delete/route.ts
+++ src/app/api/admin/users/delete/route.ts
@@ -1,14 +1,17 @@
 import { dbConnect } from "@/db/connection";
+import userModel from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 
 dbConnect();
 
 export const DELETE = async (request: NextRequest, response: NextResponse) => {
     try {
-        const {id} = await request.json();
-        console.log(id);
+        const {_id} = await request.json();
+        const deletedUser = await userModel.findByIdAndDelete(_id);
+        console.log(deletedUser)
         return NextResponse.json({
-            data: id,
+            status: 200,
+            message: "User deleted Successfully"
         });
     } catch (error) {
         console.log("error from admin/users/delete/route", error);
